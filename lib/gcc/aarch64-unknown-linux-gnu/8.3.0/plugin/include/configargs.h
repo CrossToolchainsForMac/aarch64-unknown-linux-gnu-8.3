1194c4553141d6cf58f6b45dc8d99ff8952aaffb
Zhang Hao <zhanghao@rock-chips.com>
2020-09-09 08:31:38 +0800

    init repo

---
+++ lib/gcc/aarch64-unknown-linux-gnu/8.3.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/Volumes/AOSP/arm-none-linux-eabi/.build/aarch64-unknown-linux-gnu/src/gcc/configure --build=x86_64-build_apple-darwin19.6.0 --host=x86_64-build_apple-darwin19.6.0 --target=aarch64-unknown-linux-gnu --prefix=/Volumes/AOSP/x-tools/aarch64-unknown-linux-gnu --with-sysroot=/Volumes/AOSP/x-tools/aarch64-unknown-linux-gnu/aarch64-unknown-linux-gnu/sysroot --enable-languages=c,c++ --with-pkgversion='crosstool-NG 1.24.0' --enable-__cxa_atexit --disable-libmudflap --disable-libgomp --disable-libssp --disable-libquadmath --disable-libquadmath-support --disable-libsanitizer --disable-libmpx --with-gmp=/Volumes/AOSP/arm-none-linux-eabi/.build/aarch64-unknown-linux-gnu/buildtools --with-mpfr=/Volumes/AOSP/arm-none-linux-eabi/.build/aarch64-unknown-linux-gnu/buildtools --with-mpc=/Volumes/AOSP/arm-none-linux-eabi/.build/aarch64-unknown-linux-gnu/buildtools --with-isl=/Volumes/AOSP/arm-none-linux-eabi/.build/aarch64-unknown-linux-gnu/buildtools --enable-lto --enable-threads=posix --enable-target-optspace --enable-plugin --enable-gold --disable-nls --disable-multilib --with-local-prefix=/Volumes/AOSP/x-tools/aarch64-unknown-linux-gnu/aarch64-unknown-linux-gnu/sysroot --enable-long-long";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
